2ab44e3f90aef0d50cfa37ede7d0c5abec4b2f44
Douglas Barlow <d.barlow@laposte.net>
2021-04-07 15:33:08 +0200

    Voiture

--- TD/voiture.php
+++ TD/voiture.php
@@ -1,29 +1,45 @@
+
 <?php
 class Voiture {
 
-    public $Couleur;
-    public $Marque;
-    public $Immat;   
-   
-    function setCouleur($Couleur) {
-        $this->Couleur=$Couleur;
-    }
-    function getCouleur() {
-        return $Couleur;
-    }
-       
-    function setMarque($Marque) {
-        $this->Marque=$Marque;
-    }
-    function getMarque() {
-        return $Marque;
-    }
+ private $marque;
+ private $couleur;
+ private $immatriculation;
 
-    function setImmat($Immat) {
-        $this->Immat=$Immat;
-    }
-    function getImmat() {
-        return $Immat;
-    }
+function setCouleur($couleur) {
+    $this->couleur=$couleur;
+}
+
+function getCouleur() {
+    return $this->couleur;
+}
+
+function setMarque($marque) {
+    $this->marque=$marque;
+}
+
+function getMarque() {
+    return $this->marque;
+}
+
+function setImmatriculation($immatriculation) {
+    $this->immatriculation=$immatriculation;
+}
+
+function getImmat() {
+    return $this->immatriculation;
+}
+
+ // un constructeur
+ public function __construct($m, $c, $i) {
+ $this->marque = $m;
+ $this->couleur = $c;
+ $this->immatriculation = $i;
+ }
+
+ // une methode d'affichage.
+ public function afficher() {
+ // À compléter dans le prochain exercice
+ }
 }
 ?>
