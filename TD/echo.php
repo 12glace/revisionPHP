d435962fcff5121a58bd7b9a24d41a8db3f4ab72
Douglas Barlow <d.barlow@laposte.net>
2021-04-07 15:03:27 +0200

    Exo 5 fini

--- TD/echo.php
+++ TD/echo.php
@@ -24,13 +24,37 @@
 // echo "$prenom";
 
 #EXO 5
-$marque ='Fiat';
-$couleur = 'rouge' ;
-$immatriculation = '3451 QM 86';
+// $marque ='Fiat';
+// $couleur = 'rouge' ;
+// $immatriculation = '3451 QM 86';
+// echo "<p> Voiture $immatriculation de marque $marque (couleur $couleur) </p>";
 
-echo "<p> Voiture $immatriculation de marque $marque (couleur $couleur) </p>";
+    ##Partie 3
+$voiture =[
+    '0'=>['marque'=>'Fiat',
+        'couleur' => 'rouge',
+        'immatriculation' => '3451 QM 86'],
 
+    '1'=>['marque'=>'LandRover',
+        'couleur' => 'Blue',
+        'immatriculation' => 'jspLeNum'],
 
- ?>
+    '2'=>['marque'=>'',
+        'couleur' => '',
+        'immatriculation' => '']
+
+];
+// echo "<p> Voiture ".$voiture['immatriculation']." de marque ".$voiture['marque']." (couleur ".$voiture['couleur'].") </p>";
+echo '<ul>';
+$cars=count($voiture);
+for($i=0;$i < $cars;$i++){    
+    if($voiture[$i]['marque']){
+        echo "<li> Voiture ".$voiture[$i]['immatriculation']." de marque ".$voiture[$i]['marque']." (couleur ".$voiture[$i]['couleur'].") </li>";
+    }else{
+        echo "<li>Il n’y a aucune voiture.</li>";
+    }
+}
+echo '<ul>';
+?>
  </body>
 </html> 
