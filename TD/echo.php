eaf65cdc69ca6faa65bb609436ef406a765f6387
Douglas Barlow <d.barlow@laposte.net>
2021-04-07 14:18:18 +0200

    update names

--- TD/echo.php
+++ TD/echo.php
@@ -4,14 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> Mon premier php </title>
 </head>
-<body>
-<?php
-//proute
-$teste="proute";
-echo $teste;
-?>
-    
-</body>
-</html>
+
+ <body>
+ Voici le résultat du script PHP :
+ <?php
+ 
+ $texte = "hello mum !";
+
+ echo $texte;
+ ?>
+ </body>
+</html> 
